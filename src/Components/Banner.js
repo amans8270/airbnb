688ecd3282a2b1a5f7641c8d942af1c7ba18982e
Aman Singh <amans8270@gmail.com>
2020-09-11 16:52:27 +0530

    header , footer

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -1,14 +1,14 @@
 import React , { useState } from 'react';
 import './banner.css';
 import { Button } from "@material-ui/core";
-
+import Search from './Search';
 function Banner() {
     const[showSearch,setShowSearch]=useState(false);
     return (
         <div className='banner'>
               <div className='banner_search'>
                
-
+                {showSearch && <Search/>}
                 <Button onClick={() => setShowSearch(!showSearch)} className='banner_searchButton' variant='outlined'>
                     {showSearch ? "Hide" : "Search Dates"}
                 </Button>
---
+++ src/Components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './footer.css';
+
+function Footer() {
+    return (
+        <div className="footer">
+            <p> Kindda cool stuff here </p>
+            <p>Reaact js , Material-ui, Route,css</p>
+            
+        </div>
+    )
+}
+
+export default Footer
